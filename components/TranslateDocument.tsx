8c55f56a7600132cf2591c36802dad8adfdafe45
TheKingsident <kingsident@gmail.com>
2024-08-14 19:13:01 +0100

    feat: Add react-markdown dependency and integrate it into TranslateDocument

--- components/TranslateDocument.tsx
+++ components/TranslateDocument.tsx
@@ -22,8 +22,9 @@ import {
   
 import { Button } from "./ui/button"
 import { FormEvent, useState, useTransition } from "react";
-import { LanguagesIcon } from "lucide-react";
+import { BotIcon, LanguagesIcon } from "lucide-react";
 import { toast } from "sonner";
+import Markdown from "react-markdown";
 
 type Language = 
   | "english"
@@ -49,21 +50,21 @@ const languages: Language[] = [
   "japanese",
   "korean",
   "russian",
-  "hindi"
+  "hindi",
 ];
 
 function TranslateDocument({ doc }: { doc: Y.Doc }) {
   const [isOpen, setIsOpen] = useState(false);
   const [language, setLanguage] = useState<string>("");
-  const [summary, setSummary] = useState();
-  const [question, setQuestion] = useState();
+  const [summary, setSummary] = useState("");
+  const [question, setQuestion] = useState("");
   const [isPending, startTransition] = useTransition();
 
   const handleAskQuestion = async (e: FormEvent) => {
     e.preventDefault();
 
     startTransition(async () => {
-      const documentData = doc.getMap("document-store").toJSON();
+      const documentData = doc.get("document-store").toJSON();
 
       const res = await fetch(
         `${process.env.NEXT_PUBLIC_BASE_URL}/translateDocument`,
@@ -80,9 +81,9 @@ function TranslateDocument({ doc }: { doc: Y.Doc }) {
       );
 
       if (res.ok) {
-        const { translated } = await res.json();
+        const { translated_text } = await res.json();
 
-        setSummary(translated.summary);
+        setSummary(translated_text);
         toast.success("Transalated document summary successfully!");
       }
 
@@ -108,6 +109,18 @@ function TranslateDocument({ doc }: { doc: Y.Doc }) {
 
                 {question && <p className="mt-5 text-gray-500">Q: {question}</p>}
             </DialogHeader>
+
+            {summary && (
+              <div className="flex flex-col items-start max-h-96 overflow-y-scroll gap-2 p-5 bg-gray-100">
+                <div className="flex">
+                  <BotIcon className="w-18 flex-shrink-0" />
+                  <p className="font-bold">
+                    GPT {isPending ? "is translating..." : "says:"}
+                  </p>
+                </div>
+                <p>{isPending ? "Thinking...": <Markdown>{summary}</Markdown>}</p>
+              </div>
+            )}
 
             <form className="flex gap-2" onSubmit={handleAskQuestion}>
                 <Select
